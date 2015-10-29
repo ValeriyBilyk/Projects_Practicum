5ddd6906337a2c6d5f7f4629232261f1c0115cb0
ValeriyBilyk <bilyk1996@gmail.com>
2015-10-29 19:17:06 +0200

    task 1 ready commit

---
+++ PP_Lab_3/PP_Lab_3/Исходный код.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdio.h>
+#include <ctime>
+
+using namespace std;
+
+struct Node{
+	int value;
+	Node *left;
+	Node *right;
+};
+
+Node *root; 
+int maxElement = -9999;
+
+void insertTree(Node **aNode, int data){
+	if (!(*aNode)){		// додаємо новий елемент
+		(*aNode) = new Node;
+		(*aNode)->value = data;
+		(*aNode)->left = NULL;
+		(*aNode)->right = NULL;
+	}
+	else{
+	if (data < (*aNode)->value) insertTree(&(*aNode)->left,data);
+	else insertTree(&(*aNode)->right, data);
+	}
+	return;
+}
+
+void printTree(Node *aNode){
+	if (aNode){
+		printTree(aNode->left);
+		cout<<aNode->value<<"  ";
+		printTree(aNode->right);
+	}
+	return;
+}
+
+void findMaxElement(Node *aNode){
+	if (aNode){
+		if (aNode->value > maxElement)
+			maxElement = aNode->value;
+		findMaxElement(aNode->left);
+		findMaxElement(aNode->right);
+	}
+	return;
+}
+
+void main(){
+	srand(time(NULL));
+	setlocale(LC_CTYPE,"rus");
+	for(int i = 0;i < 10;i++){
+		insertTree(&root,rand() % 50 - 25);
+	}
+	cout << "Елементи дерева: " << endl;
+	printTree(root);
+	findMaxElement(root);
+	cout << endl << endl << "Найбiльший елемент дерева = " << maxElement << endl;
+	cout << endl;
+	system("pause");
+}
